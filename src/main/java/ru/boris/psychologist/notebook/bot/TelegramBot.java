a1809bc13d759b4ac8705d749420cc1828311f6f
Тютин Борис Сергеевич <btyutin@phoenixit.ru>
2024-04-06 14:40:43 +0300

    add send message

--- src/main/java/ru/boris/psychologist/notebook/bot/TelegramBot.java
+++ src/main/java/ru/boris/psychologist/notebook/bot/TelegramBot.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.boris.psychologist.notebook.config.BotConfig;
 
 @Slf4j
@@ -17,6 +19,8 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         log.info(update.toString());
+
+        send(update.getMessage().getChatId());
     }
 
     @Override
@@ -33,5 +37,17 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onRegister() {
         log.info("Bot was registered");
         super.onRegister();
+    }
+
+    private void send(Long chatId){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText("Hello Boris");
+        sendMessage.setChatId(chatId);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
